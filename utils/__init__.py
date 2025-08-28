b7d079f742eddbcf8a20d171a478888d4cdb5120
daizihan233 <y066ydg5@duck.com>
2025-08-29 02:52:51 +0800

    refactor: 重命名 DNS 提供者模块并优化导入路径

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from utils.nodes import list_node
-from utils.alidns import AliDns
-from utils.cloudflare import CloudflareDns
+from utils.provider.alidns import AliDns
+from utils.provider.cloudflare import CloudflareDns
 from utils.base import BaseDns
 # 这东西也需要注释吗？
